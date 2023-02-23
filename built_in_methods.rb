be0f60efd6960bacdb1d00bc618060ebae35cf12
Ashley Plasket <ashley.plasket@gmail.com>
2023-02-23 18:49:58 -0500

    Add descriptions to section 1

--- built_in_methods.rb
+++ built_in_methods.rb
@@ -6,22 +6,32 @@
 # The downcase method is called on the string object "Hello World"
 # No arguments are passed; downcase has one clear job which is to lowercase all letters that exist in the String
 # The return value is "hello world"
-"Hello World".downcase
+p "Hello World".downcase
 
+# The include method is called on the string object "Hellow World"
+# The include? method returns true if the data in the sting object includes the argument passed in.
+# The return value is true, because "Hello World" does include "Hello"
+p "Hello World".include?("Hello")
 
-"Hello World".include?("Hello")
+# The end_with? method is called on the string object "Hello World"
+# The end_with? method returns true if the data in the string object ends_with the argument passed in.
+# The return value in this example is false, because "Hello World" does not end with "Hello"
+p "Hello World".end_with?("Hello")
 
+# The end_with? method is called on the string object "Hello World"
+# The end_with? method returns true if the data in the string object ends_with the argument passed in.
+# The return value in this example is true, because "Hello World" does end with "rld"
+p "Hello World".end_with?("rld")
 
-"Hello World".end_with?("Hello")
+# The even? method is called on the integer object 12
+# The even? method returns true if the data in the integer is even.
+# The return value in this example is true, because 12 is even
+p 12.even?
 
-
-"Hello World".end_with?("rld")
-
-
-12.even?
-
-
-18.next
+# The next method is called on the integer object 18
+# The next method returns the successor integor (or integer + 1)
+# The return value in this example is 19, since the the successor to 18 (or 18 plus 1) equals 19
+p 18.next
 
 
 
@@ -39,7 +49,12 @@
 first_name = "Jeff"
 puts first_name.start_with?("J")
 
-
+# The include? method is called on the email variable, which stores the string object "ashley.plasket@gmail.com". 
+# The include? method returns true if the data in the email variable includes the argument passed in.
+# In this example, the return value is true, because email does include "@".
+# The puts command prints the return value of the include? method (true) to the console.
+email = "ashley.plasket@gmail.com"
+puts email.include? ("@")
 
 # SECTION 3: Calling methods on variables assigned to integers.
 # Declare 2 variables assigned to integer objects.
