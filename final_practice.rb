fb5adf62dd240d53c844c6fdd13fce64571bfc69
Ashley Plasket <ashley.plasket@gmail.com>
2023-02-23 20:49:48 -0500

    Add methods for #4 and #5

--- final_practice.rb
+++ final_practice.rb
@@ -36,48 +36,55 @@ p greeting4
 
 
 # 3: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
+
 def greet_person(first, middle, last)
-   "Howdy, #{first} #{middle} #{last}!"
-end
+    "Howdy, #{first} #{middle} #{last}!"
+ end
+ 
+p greet_person("Chandler", "Mystery", "Bing")
 
-first = "Chandler"
-middle = "Mystery"
-last = "Bing"
-
-p greet_person(first, middle, last)
-
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+# What is the return value of your method? a string with a name interpolated into "Howdy, #{first} #{middle} #{last}!"
+# How many arguments did you pass your method? 3
+# What data type was your argument(s)? string object
 
 
 # 4: Write a method named square that takes in one integer, and returns the square of that integer.
 # Bonus: Print a sentence that interpolates the return value of your square method.
-# def square 
+def square(num)
+    num * num
+end
 
+p square(4)
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+# What is the return value of your method? 16
+# How many arguments did you pass your method? 1
+# What data type was your argument(s)? integer object
 
 
 # 5: Write a method named check_stock that satisfies the following interaction pattern:
 # Hint: You will only write one check_stock method that checks the quantity and then prints the corresponding statement.
 
-# check_stock(4, "Coffee");
-# # => "Coffee is stocked"
-
-# check_stock(3, "Tortillas");
-# # => "Tortillas - running LOW"
-
-# check_stock(0, "Cheese");
-# # => "Cheese - OUT of stock!"
-
-# check_stock(1, "Salsa");
-# # => "Salsa - running LOW"
 
 def check_stock(num, item)
-if num == 4
-    puts "coffee is stocked"
+    if num >= 4
+        puts "#{item} is stocked"
+    elsif num > 0 && num < 4
+        puts "#{item} - running LOW"
+    elsif num == 0
+        puts "#{item} - OUT of stock!"
+    end
 end
-end
+
+check_stock(4, "Coffee");
+# => "Coffee is stocked"
+
+check_stock(3, "Tortillas");
+# => "Tortillas - running LOW"
+
+check_stock(0, "Cheese");
+# => "Cheese - OUT of stock!"
+
+check_stock(1, "Salsa");
+# => "Salsa - running LOW"
+
+check_stock(1, "peanuts")
